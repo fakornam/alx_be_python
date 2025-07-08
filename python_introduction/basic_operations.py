300cc2033bc68453bb0e7bf1ffe5e9aec8a5c698
fakornam <benmorladza16@gmail.com>
2025-07-08 08:38:02 +0000

    Relocate basic_operation.py into the python_introduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1=10 
+number2=5
+Addition= number1 + number2
+subtraction= number1 - number2
+Multiplation= number2 * number1
+
+print(f"Addition of {number1} and {number2} is {Addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number2} and {number1} is {Multiplation}")
