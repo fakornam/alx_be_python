83fc0ea5d3fb945e03e39e336da69e5c6ed830fb
fakornam <benmorladza16@gmail.com>
2025-07-25 07:47:05 +0000

    Perform Operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+def perform_operation(num1: float, num2: float, operation: str):
+    if operation in ['add', '+']:
+        return num1 + num2
+    elif operation in ['subtract', '-']:
+        return num1 - num2
+    elif operation in ['multiply', '*']:
+        return num1 * num2
+    elif operation in ['divide', '/']:
+        if num2 == 0:
+            return "Error: Division by zero is undefined."
+        return num1 / num2
+    else:
+        return "Error: Unsupported operation."
