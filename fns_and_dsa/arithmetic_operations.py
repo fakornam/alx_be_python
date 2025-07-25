d4e79007bd0123bee2ace8319c1c95537007f7a5
fakornam <benmorladza16@gmail.com>
2025-07-25 07:50:59 +0000

    Performing basic Operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,11 @@
 def perform_operation(num1: float, num2: float, operation: str):
-    if operation in ['add', '+']:
+    if operation in 'add':
         return num1 + num2
-    elif operation in ['subtract', '-']:
+    elif operation in 'subtract':
         return num1 - num2
-    elif operation in ['multiply', '*']:
+    elif operation in 'multiply' :
         return num1 * num2
-    elif operation in ['divide', '/']:
+    elif operation in 'divide':
         if num2 == 0:
             return "Error: Division by zero is undefined."
         return num1 / num2
