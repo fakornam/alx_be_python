455c256cfdf8cff15f1cacb92e445afc0ea73008
fakornam <benmorladza16@gmail.com>
2025-08-07 09:06:52 +0000

    Book store created

---
+++ oop/book_class.py
@@ -0,0 +1,13 @@
+class Book:
+    def __init__(self, title:str, author:str, year:int):
+         self.title = title
+         self.author = author
+         self.year = year
+    def __del__(self):
+        print(f"Deleting '{self.title}' ")
+    
+    def __str__(self):
+        return "'{self.title}' by {self.author}, published in {self.year}"
+    
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
