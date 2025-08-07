4d44062c1862c54eadac4d996aa8e20db0341aa6
fakornam <benmorladza16@gmail.com>
2025-08-07 09:14:18 +0000

    Book store created

--- oop/book_class.py
+++ oop/book_class.py
@@ -3,11 +3,12 @@ class Book:
          self.title = title
          self.author = author
          self.year = year
-    def __del__(self):
-        print(f"Deleting {self.title}")
     
     def __str__(self):
         return "{self.title} by {self.author}, published in {self.year}"
     
     def __repr__(self):
-        return f"Book({self.title}, {self.author}, {self.year})"
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+    
+    def __del__(self):
+        print(f"Deleting {self.title}")
