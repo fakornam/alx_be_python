279d8127381f05a0cf6167e42924e1d22333cbd7
fakornam <benmorladza16@gmail.com>
2025-08-08 08:42:29 +0000

    library system with Ebook and Print Book

---
+++ oop/library_system.py
@@ -0,0 +1,45 @@
+# library_system.py
+
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def get_details(self):
+        return f"{self.title} by {self.author}"
+
+
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size  # in MB
+
+    def get_details(self):
+        return f"{self.title} by {self.author} [EBook, {self.file_size}MB]"
+
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def get_details(self):
+        return f"{self.title} by {self.author} [PrintBook, {self.page_count} pages]"
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        if isinstance(book, Book):
+            self.books.append(book)
+        else:
+            print("Only Book instances can be added.")
+
+    def list_books(self):
+        if not self.books:
+            print("Library is empty.")
+        else:
+            for book in self.books:
+                print(book.get_details())
